03b1f899f4d7e3f5655113e99275a177c9b7f98e
Kelver Machado <kelver_kmv@hotmail.com>
2019-07-10 12:38:21 -0400

    testes

--- hW2.py
+++ hW2.py
@@ -4,24 +4,22 @@ import cv2
 IMAGE_FILE = '4.jpg' # Change this to be your image
 
 # Cascade file
-CASCADE_FILE = 'cascadefina.xml'
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('cascadefina.xml')
 
 # Load image file
-image = cv2.imread(IMAGE_FILE)
+img = cv2.imread(IMAGE_FILE)
 
 # Convert the image to gray
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-color = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-# Load your cascade file
-cascade = cv2.CascadeClassifier(CASCADE_FILE)
-eyes = cascade.detectMultiScale(gray)
-
-for (ex,ey,ew,eh) in eyes:
-	cv2.rectangle(color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-# Display the cascade to the user
-cv2.imshow("s", image)
-cv2.imwrite("s.jpg", image)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
